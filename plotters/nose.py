0b877bac8f1abb1eb8b22090b871381e908018db
dante <dantbarbieri@gmail.com>
2024-04-16 16:23:04 -0300

    modificacion final

--- plotters/nose.py
+++ plotters/nose.py
@@ -4,15 +4,15 @@ from gapminder import gapminder
 
 def plot():
     figura = (
-        so.Plot(datos_oceania,x="year",y="lifeExp")
+        so.Plot(datos_oceania,x="year",y="lifeExp",color="country")
     .add(so.Dot())
-    .add(so.Lines(color="#bbca"))
+    .add(so.Lines(color=".7"))
     .add(so.Line(), so.PolyFit(1))
     .facet("country")
         )
     )
     return dict(
-        descripcion="Un sofisticado gráfico con la expectativa de vida de cada pais de Oceania en funcion del tiempo",
-        autor="Dante Barbieri",
+        descripcion="Un hermoso gráfico con la expectativa de vida de cada pais de Oceania en funcion del tiempo",
+        autor="Dante Barbieri, el original",
         figura=figura,
     )
