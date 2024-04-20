8737350c6f47df9284bd1aea12121b60be43b753
Ana Rius <anaflorazul@gmail.com~>
2024-04-20 11:31:56 -0300

    funcion hecha

--- plotters/tpcito.py
+++ plotters/tpcito.py
@@ -1,19 +1,35 @@
-import seaborn.objects as so
+import seaborn as sns
+import pandas as pd
 from gapminder import gapminder
 
 
 def plot():
-    figura = (
-        so.Plot(gapminder[["continent", "country"]].drop_duplicates(), x="continent")
-        .add(so.Bar(), so.Hist())
-        .label(
-            title="Cantidad de países por continente",
-            x="Continente",
-            y="Cantidad de países",
+   df = pd.DataFrame(gapminder)
+   paises = ['Argentina','Brazil','Bolivia','Chile','Colombia','Paraguay','Peru','Uruguay','Venezuela']
+   df = df[df['country'].isin(paises)]
+
+   sns.set_theme(style="dark")
+
+   g = sns.relplot(
+        data=df,
+        x="year", y="lifeExp", col="country", hue="country",
+        kind="line", palette="crest", linewidth=4, zorder=5,
+        col_wrap=3, height=3, aspect=1.5, legend=False,
         )
+   for year, ax in g.axes_dict.items():
+        ax.text(.4, 1, year, transform=ax.transAxes, fontweight="bold")
+
+        sns.lineplot(
+            data=df, x="year", y="lifeExp", units="country",
+           estimator=None, color=".8", linewidth=1, ax=ax,
+       )
+
+   g.set_titles("")
+   g.set_axis_labels("", "LifeExp")
+   g.tight_layout()
+   return dict(
+        descripcion="Evolucion de la espectativa de vida en paises de Sur America",
+        autor="Ana Rius",
+        figura=g,
     )
-    return dict(
-        descripcion="Un sofisticado gráfico con el número de países en cada continente",
-        autor="La cátedra",
-        figura=figura,
-    )
+
