8f3f5a064ab189a6d5fbdb18afd1128067b3c384
Franchu <74627280+franvalli@users.noreply.github.com>
2024-04-23 19:23:51 -0300

    Update tp0-Valli.py

--- plotters/tp0-Valli.py
+++ plotters/tp0-Valli.py
@@ -1,20 +1,22 @@
 import seaborn.objects as so
 from gapminder import gapminder
 
+datosArg = gapminder[gapminder["country"]=="Argentina"]
+datosChile = gapminder[gapminder["country"]=="Chile"]
 
 def plot():
     figura = (
-        so.Plot(gapminder[gapminder["country"]=="Argentina"], x="year", y = "gdpPercap")
-        .add(so.Line())
-        .add(so.Dot())
+        so.Plot(data = datosArg, x = "year", color = "country")
+        .add(so.Line(), y = datosChile.gdpPercap)
+        .add(so.Line(), y = datosArg.gdpPercap)
         .label(
-            title="El PBI per capita de Argentina",
+            title="El PBI per capita de Argentina y Chile",
             x="Año",
             y="PBI per capita",
         )
     )
     return dict(
-        descripcion="El PBI per capita de Argentina a través de los años",
+        descripcion="Comparación del PBI per capita de Argentina y Chile a través de los años",
         autor="Francisca Valli",
         figura=figura,
     )
