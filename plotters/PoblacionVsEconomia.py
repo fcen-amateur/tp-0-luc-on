7603ef8542feff4223d5ddd11cbaeafcfc692bc9
Dani Bustos <danielbustoslangmandel@gmail.com>
2024-04-26 23:32:22 -0300

    cambios al 2do grafico

--- plotters/PoblacionVsEconomia.py
+++ plotters/PoblacionVsEconomia.py
@@ -7,21 +7,21 @@ import pandas as pd
 def plot():
     
    
-    paises_objetivo = ["Brazil","Argentina","Chile","Mexico","China","United States","India","Germany","Italy","Japan"]
-    paises = gapminder[ (gapminder.country.isin(paises_objetivo)) &(gapminder.year > 1980)]
+    paises_objetivo = []
+    paises = gapminder[(gapminder.year >= 2004) & ~(gapminder.country.isin(paises_objetivo))]
 
-
-    figura = (so.Plot(paises,"year",y = paises["gdpPercap"]*paises["pop"],color = "country",pointsize = "pop")
+    figura = (so.Plot(paises,"pop",y = paises["gdpPercap"]*paises["pop"],color = "continent")
+        .add(so.Line(),so.PolyFit(1))
         .add(so.Dot())
         .label(title="PBI vs Poblacion",
-            x="Año",
+            x="Poblacion",
             y="Producto Bruto Interno")
-             
-        ).scale(pointsize = (1,20))
+         .scale()    
+        ).facet("continent",wrap= 5).share(y = False)
 
     
     return dict(
-        descripcion="Contario a lo que la intuicion nos diria, no hay una correlacion directa entre la poblacion y el tamaño de una economia. En la modernidad, el acceso a la tecnologia,capitales,educacion junto con la existencia de robustaz instituciones nacionales tienen mucho mas relevancia para las economias que el tamaño de sus poblaciones",
+        descripcion="Gran caso de la paradoja de Simpson. Si miramos la relacion del pbi con la poblacion de los paises, en cada continente parece habe una correlacion directa entre los mismos. Sin embargo, contrario a lo que la intuicion nos diria, no hay una correlacion directa entre la poblacion y el tamaño de una economia a nivel global.Por ejemplo, india y china tienen casi 4 veces la poblacion de Estados Unidos, sin embargo si sumamos sus pbis y los comparamos al pais americano, no llegan a superarlo .En la modernidad, el acceso a la tecnologia,capitales,educacion junto con la existencia de robustaz instituciones nacionales tienen mucho mas relevancia para las economias que el tamaño de sus poblaciones",
         autor="Daniel Bustos",
         figura=figura,
     )
